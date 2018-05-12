9c489d60ad18b6114275573d155854645f529e61
joahra <ahra@philo-biz.com>
2018-05-12 20:10:25 +0900

    Updated.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
+//    모바일 gnb 햄버거 아이콘
+    var trigger = $('#hamburger'),
+        isClosed = true;
+
+    trigger.click(function () {
+      burgerTime();
+    });
+
+    function burgerTime() {
+      if (isClosed == true) {
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        $('.gnb').addClass('active');
+        isClosed = false;
+      } else {
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        $('.gnb').removeClass('active');
+        isClosed = true;
+      }
+    };
     
 //    skill 소개
-    var val_count=[60,80,90,70];
+    var val_count=[90,90,60,100];
     chart_event_1(val_count,'chart-area1');
     
     function chart_event_1(val_1,canvasId){
@@ -10,31 +31,34 @@ $(document).ready(function() {
             data: {
                 datasets: [{
                     data: [val_1[0],val_1[1],val_1[2],val_1[3]],
-                    backgroundColor: ["rgba(255,133,207,0.2)","rgba(60,194,60,0.2)","rgba(255,255,0,0.2)",
-                        "rgba(0,150,255,0.2)"],
+                    backgroundColor: ["rgba(255,133,207,0.5)","rgba(60,194,60,0.3)","rgba(255,255,0,0.3)",
+                        "rgba(0,150,255,0.3)"],
                     label: 'My dataset' // for legend
                 }],
                 labels: [
-                    "Red",
-                    "Orange",
-                    "Yellow",
-                    "Green"
+                    "HTML(5)/CSS(3)",
+                    "JavaScript/JQuery",
+                    "Photoshop",
+                    "성실함"
                 ]
             },
             options: {
-                responsive: true,
+                maintainAspectRatio: false,
                 legend: {
-                    position: 'right',
+                    position: 'bottom',
                 },
-                title: {
-                    display: true,
-                    text: 'Chart.js Polar Area Chart'
+                layout: {
+                    padding: {
+                        top: 20,
+                    }
                 },
                 scale: {
-                  ticks: {
-                    beginAtZero: true
-                  },
-                  reverse: false
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 50,
+                        backdropColor:'transparent',
+                        fontColor:"transparent",
+                   }
                 },
                 animation: {
                     animateRotate: false,
@@ -42,20 +66,41 @@ $(document).ready(function() {
                 }
             }
         };
-//          Chart.defaults.global.tooltips.titleFontSize = 14;
-//          Chart.defaults.global.tooltips.bodyFontSize = 14;
-//          Chart.defaults.global.tooltips.titleMarginBottom = 10;
-//          Chart.defaults.global.elements.line.tension = .01;
-//          Chart.defaults.global.elements.point.hoverRadius = 10;
-//          Chart.defaults.global.elements.point.hitRadius = 3;
-//          Chart.defaults.global.legend.display = false;
-//          Chart.defaults.scale.lineArc = false;
-//
-//          Chart.defaults.scale.gridLines.color = "#e5e5e5";
-//          Chart.defaults.scale.gridLines.zeroLineColor = "#e5e5e5";
+        Chart.defaults.scale.gridLines.color = "#eee";
         window.myPolarArea = new Chart(document.getElementById(canvasId), config);
+    };
 
-    }
+//    scroll 이벤트
+    var section1_top=$('.section1').position().top;
+    var section2_top=$('.section2').position().top;
+    var section3_top=$('.section3').position().top;
+    var pos=1;
     
+    $(window).on('scroll',function(){
+        var win_top=$(this).scrollTop();
+        section2_top=$('.section2').offset().top-300;
+        section3_top=$('.section3').offset().top-300;
+//        console.log(win_top,section1_top,section2_top,section3_top)
+        if(win_top<section2_top){
+            pos=0;
+        }else if(win_top>=section2_top && win_top<section3_top){
+            pos=1;
+        }else{
+            pos=2;
+        }
+            gnb_event();
+    });
+    function gnb_event(){
+        $('.gnb li').removeClass('on');
+        $('.gnb li').eq(pos).addClass('on');
+    };
     
+    $('.gnb a').on('click',function(e){
+        e.preventDefault();
+        var $index=$(this).parent().index()+1;
+//        console.log($index)
+        var target=$('.section'+$index).position().top;
+
+        $('html').animate({'scrollTop':target});
+    });
 })
